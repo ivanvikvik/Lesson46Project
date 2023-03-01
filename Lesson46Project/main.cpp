1e4d48206e43426a9eb8630fb4720b46cdb17cee
ivanvikvik <ivanvikvik@gmail.com>
2023-03-01 18:14:57 +0300

    Change text from eng to rus

--- Lesson46Project/main.cpp
+++ Lesson46Project/main.cpp
@@ -7,11 +7,11 @@ void rand_init(int* vector, int size, int bound);
 int max(int* vector, int size);
 int min(int* vector, int size);
 
-
+// стартовая точка в приложение
 int main() {
 	int size;
 
-	cout << "Input size of vector: ";
+	cout << "Введите размерность вектора: ";
 	cin >> size;
 
 	int* vector = new int[size];
